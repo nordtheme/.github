e77e7bfd96ea1a327de55e4ebd9c6ac19c8fbe87
Sven Greb <development@svengreb.de>
2023-04-30 23:45:49 +0200

    Adapt remaining `arcticicestudio` "fade-out" changes
    With the retirement of the "Arctic Ice Studio" personal & Nord project
    brand [1] various migrations to the `svengreb` GitHub account [2] of
    other projects were also completed within the last weeks. There are also
    changes in Nord projects like `nordtheme/web` that other projects
    depended on and can now be adapted as well.
    Therefore all Nord projects must adapt these changes which includes...
    
    - the migration and repository name change of
      `arcticicestudio/nord-docs`[2] - all references must be updated.
    - the migration of the `arcticicestudio/styleguide-git` repository to
      the `svengreb` "real-in-person" identity [3] - all references and
      used npm packages must be updated.
    - the migration of the `arcticicestudio/styleguide-javascript`
      repository to the `svengreb` "real-in-person" identity [4] - all
      references and used npm packages must be updated.
    - the migration of the `arcticicestudio/styleguide-markdown` repository
      to the `svengreb` "real-in-person" identity [5] - all references and
      used npm packages must be updated.
    
    [1]: https://github.com/orgs/nordtheme/discussions/183#retire-arctic-ice-studio-as-nord-brand
    [2]: https://github.com/nordtheme/web/issues/241
    [3]: https://github.com/svengreb/styleguide-git/issues/12
    [4]: https://github.com/svengreb/styleguide-javascript/issues/88
    [5]: https://github.com/svengreb/styleguide-markdown/issues/63
    
    GH-26

---
+++ .remarkrc.mjs
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016-present Sven Greb <development@svengreb.de>
+ * This source code is licensed under the Creative Commons Attribution-ShareAlike 4.0 International license found in the license file.
+ */
+
+/**
+ * Configurations for remark-lint.
+ * @see https://github.com/remarkjs/remark-lint
+ * @see https://remark.js.org
+ */
+export default {
+  plugins: ["@svengreb/remark-preset-lint"],
+};
